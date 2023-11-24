ea07f5ba7be309c45e50239d1de659fce065b82e
Nicolas Fiascaro <n.fiascaro@gmail.com>
2023-11-24 15:13:00 +0100

    Fix navigation

--- src/app/ui/Navigation/Navigation.tsx
+++ src/app/ui/Navigation/Navigation.tsx
@@ -2,14 +2,19 @@
 import { PrismicLink } from "@prismicio/react"
 import { usePathname } from "next/navigation"
 import clsx from "clsx"
+import { NavigationDocument } from "../../../../prismicio-types"
+import { asLink } from "@prismicio/client"
 
-export const Navigation = ({ data }) => {
+export const Navigation = ({ data }: NavigationDocument<"navigation">) => {
+  console.log("🚀 ~ file: Navigation.tsx:8 ~ Navigation ~ data:", data.slices)
+
   const pathname = usePathname()
   return (
     <nav>
       <ul className="flex gap-8">
         {data.slices.map((item) => {
-          const url = item.primary.link.url
+          // Fix TS Prismic link management. see: https://community.prismic.io/t/how-to-get-url-from-link-in-typescript-next-js-project/12270/4
+          const url = asLink(item.primary.link) as string
           return (
             <li
               key={JSON.stringify(item)}
