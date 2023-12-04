840259f4715796cbe39c29dbda8ce4691f72233f
Nicolas Fiascaro <n.fiascaro@gmail.com>
2023-12-04 14:38:47 +0100

    Fix conflicting next conf vs middleware

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-const prismic = require("@prismicio/client");
-const sm = require("./slicemachine.config.json");
-
-/** @returns {Promise<import('next').NextConfig>} */
-module.exports = async () => {
-  const client = prismic.createClient(sm.repositoryName);
-
-  const repository = await client.getRepository();
-  const locales = repository.languages.map((lang) => lang.id);
-
-
-  return {
-    reactStrictMode: true,
-    i18n: {
-      locales,
-      // This is the default locale. It will not be included in URLs.
-      defaultLocale: locales[0],
-      localeDetection: false
-    },
-  };
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
