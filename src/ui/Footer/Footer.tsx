17fc055d289cc6892db16f6cb4ea9d374c8c4ab8
Nicolas Fiascaro <n.fiascaro@gmail.com>
2023-12-07 10:26:05 +0100

    Render Carbon Badge only in production

--- src/ui/Footer/Footer.tsx
+++ src/ui/Footer/Footer.tsx
@@ -4,8 +4,14 @@ export const Footer = () => {
   return (
     <footer className="px-6 py-2 md:py-4">
       <div className="mx-auto w-full max-w-6xl">
-        <div id="wcb" className="carbonbadge wcb-d"></div>
-        <Script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" />
+        {process.env.NODE_ENV === "production" ? (
+          <>
+            <div id="wcb" className="carbonbadge wcb-d"></div>
+            <Script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" />
+          </>
+        ) : (
+          <div className="font-bold text-center block">ENV DEV</div>
+        )}
       </div>
     </footer>
   )
