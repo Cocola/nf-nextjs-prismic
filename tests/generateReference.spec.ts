892cfbf41e93436af2ff753c2536b91dce8f0b67
Nicolas Fiascaro <n.fiascaro@gmail.com>
2024-09-03 16:44:56 +0200

    Init visual regression tests

---
+++ tests/generateReference.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('Generate homepage reference screenshot', async ({ page }) => {
+  await page.goto('https://www.nicolas-fiascaro.com/');
+  await page.waitForTimeout(2000);
+
+  await expect(page).toHaveScreenshot({
+    path: 'screenshots/homepage-reference.png',
+  });
+});
